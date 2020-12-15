d280a35a2c93639e3346e01d89646acd99154049
JD Martinez <focus4ursoul@gmail.com>
2020-12-15 09:48:12 -0800

    create jsx structure for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,16 @@ export default function Header() {
         <header className="container">
             <img className="headerLogo" src="/images/meneds_logo.png" alt="meneds logo" />
 
-            {/* <ul>
-                <li>Menu</li>
-                <li>Location</li>
-            </ul> */}
+            <ul className="navList">
+                <li className="navItem">
+                    <a className="navLink" href="#">
+                        Menu
+                    </a>
+                </li>
+                <li className="navItem">
+                    <a className="navLink" href="#">Location</a>
+                </li>
+            </ul>
         </header >
     )
 }

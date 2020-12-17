cff9015c8f3691831209b6070e0e04aadca34215
JD Martinez <focus4ursoul@gmail.com>
2020-12-17 15:13:16 -0800

    create responsive markers fr smll scrn

---
+++ src/components/subComponents/MenuMarkers.js
@@ -0,0 +1,13 @@
+
+export default function menuMarkers() {
+    return (
+        <div className="menuMarkerList">
+            <h2 className="displayA">Cheese Only</h2>
+            <h2 className="displayB">1 Topping</h2>
+            <h2 className="displayC">2 Topping</h2>
+            <h2 className="displayD">3 Topping</h2>
+            <h2 className="displayE">Signature Sides</h2>
+            <h2 className="displayF">Additional Toppings</h2>
+        </div>
+    )
+}

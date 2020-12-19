20b85de2902761d5973e805e6d1bf4bc6562a63c
JD Martinez <focus4ursoul@gmail.com>
2020-12-19 15:56:50 -0800

    add signature series section

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,51 +1,51 @@
 export default function Featured() {
     return (
-        <section>
-            <div className="feature">
-                <img />
-                <h2>SIDES</h2>
-                <ul>
+        <section className="feature">
+            <div className="featureContainer">
+                <div>
+                    <img className="featuredImages" src="/images/bites.png" alt="cheesy bread" />
+                </div>
+                <ul className="featuredDetailsContainer">
+                    <h2 className="featuredSubTitle menuSubTitle hasAfter">SIDES</h2>
                     <li>GARLIC BREAD 5.00</li>
                     <li>GARLIC CHEESE BREAD 6.00</li>
                     <li>GARLIC CHEESY BITES 6.00</li>
                     <li>TOSSED SALAD 3.25</li>
                     <li>POTATO WEDGES</li>
-                    <li>COMING SOON</li>
+                    <li className="featuredItemInfo">COMING SOON</li>
                     <li>WINGS</li>
-                    <li>COMING SOON</li>
+                    <li className="featuredItemInfo">COMING SOON</li>
                     <li>HOME-MADE RANCH  .63</li>
                     <li>JALAPEÑOS .63</li>
                 </ul>
             </div>
-            <div className="feature">
-                <img />
-                <h2>SANDWICHES</h2>
-                <ul>
+            <div className="featureContainer reverseWrap">
+                <div>
+                    <img className="featuredImages" src="/images/sub.png" alt="sub sandwich" />
+                </div>
+                <ul className="featuredDetailsContainer">
+                    <h2 className="featuredSubTitle menuSubTitle">SANDWICHES</h2>
                     <li>SUB SANDWICH 6.00</li>
                     <li>GARLIC CHEESE BREAD 6.00</li>
                     <li>HAM CHEESE 6.00</li>
                     <li>PIZZA SANDWICH 6.25</li>
                 </ul>
             </div>
-            <div className="feature">
-                <img />
-                <h2>LUNCH SPECIALS</h2>
-                <ul>
+            <div className="featureContainer">
+                <div>
+                    <img className="featuredImages" src="/images/pepperoni_box.png" alt="pepperoni pizza" />
+                </div>
+                <ul className="featuredDetailsContainer">
+                    <h2 className="featuredSubTitle menuSubTitle">LUNCH SPECIALS</h2>
                     <li>MINI PIZZA 8.50</li>
-                    <li>1-TOPPING MINI PIZZA, SALAD AND SOFT DRINK</li>
+                    <li className="featuredItemInfo">1-TOPPING MINI PIZZA, SALAD AND SOFT DRINK</li>
                     <li>ANY SANDWICH 8.50</li>
-                    <li>SANDWICH, SALAD AND SOFT DRINK</li>
-                </ul>
-            </div>
-            <div className="feature">
-                <img />
-                <h2>DESSERTS</h2>
-                <ul>
+                    <li className="featuredItemInfo">SANDWICH, SALAD AND SOFT DRINK</li>
+                    <h2 className="featuredSubTitle menuSubTitle">DESSERTS</h2>
                     <li>CINNA TREATS 5.00</li>
                     <li>MONSTER COOKIE 8.00</li>
                 </ul>
             </div>
-
         </section>
     )
 }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@ import "../../styles/header.css";
 import "../../styles/slideshow.css";
 import "../../styles/menu.css";
 import "../../styles/signature.css";
+import "../../styles/featured.css";
 
 function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />
